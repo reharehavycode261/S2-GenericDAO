26650d1af2026650be8561972af64837a9462358
rehareha261 <161926797+rehareha261@users.noreply.github.com>
2025-10-11 22:31:49 +0300

    send

--- src/database/core/GenericDAO.java
+++ src/database/core/GenericDAO.java
@@ -1,71 +1,75 @@
 package database.core;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import database.exception.SQL.AttributeMissingException;
+import database.exception.SQL.AttributeTypeNotExistingException;
+import database.exception.object.NotIdentifiedInDatabaseException;
+
+import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 
-public abstract class GenericDAO<T> {
-    protected Database database;
-    protected String tableName;
-    
-    public GenericDAO(Database database, String tableName) {
-        this.database = database;
-        this.tableName = tableName;
-    }
-    
-    protected abstract T createEntity(ResultSet rs) throws SQLException;
-    
-    public List<T> findAll() throws SQLException {
-        List<T> results = new ArrayList<>();
-        String query = "SELECT * FROM " + tableName;
-        
-        try (Connection conn = database.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query);
-             ResultSet rs = stmt.executeQuery()) {
-            
-            while (rs.next()) {
-                results.add(createEntity(rs));
-            }
-        }
-        
-        return results;
+public class GenericDAO {
+    String id;
+
+    public void createTable(DBConnection dbConnection) throws SQLException, AttributeTypeNotExistingException, AttributeMissingException {
+        dbConnection.getDatabase().createTable(dbConnection.getConnection(), this);
+        dbConnection.getDatabase().createSequence(dbConnection.getConnection(), getClass().getSimpleName()+"_seq");
     }
 
-    /**
-     * Compte le nombre total d'enregistrements dans la table.
-     * 
-     * @return le nombre total d'enregistrements
-     * @throws SQLException si une erreur SQL survient
-     */
-    public long count() throws SQLException {
-        return count(null);
+    public void save(DBConnection dbConnection, Sequence sequence) throws SQLException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        setId(dbConnection.getDatabase().getSequence(dbConnection.getConnection(), sequence));
+        dbConnection.getDatabase().insertObject(dbConnection.getConnection(), this);
     }
 
-    /**
-     * Compte le nombre d'enregistrements dans la table qui correspondent à une condition WHERE.
-     * 
-     * @param whereClause la clause WHERE SQL (sans le mot-clé "WHERE"), peut être null
-     * @return le nombre d'enregistrements correspondant aux critères
-     * @throws SQLException si une erreur SQL survient
-     */
-    public long count(String whereClause) throws SQLException {
-        StringBuilder query = new StringBuilder("SELECT COUNT(*) FROM ").append(tableName);
-        
-        if (whereClause != null && !whereClause.trim().isEmpty()) {
-            query.append(" WHERE ").append(whereClause);
-        }
-        
-        try (Connection conn = database.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query.toString());
-             ResultSet rs = stmt.executeQuery()) {
-            
-            if (rs.next()) {
-                return rs.getLong(1);
-            }
-            return 0;
-        }
+    public void save(DBConnection dbConnection) throws SQLException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        Sequence sequence = new Sequence("", 10, getClass().getSimpleName());
+        save(dbConnection, sequence);
+    }
+
+    void update(DBConnection dbConnection, String condition) throws SQLException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        dbConnection.getDatabase().updateObject(dbConnection.getConnection(), condition, this);
+    }
+
+    public void update(DBConnection dbConnection) throws SQLException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, NotIdentifiedInDatabaseException {
+        update(dbConnection,"id='"+getId()+"'");
+    }
+
+    void delete(DBConnection dbConnection, String condition) throws SQLException {
+        dbConnection.getDatabase().delete(dbConnection.getConnection(), getClass().getSimpleName(), condition);
+    }
+
+    public void delete(DBConnection dbConnection) throws SQLException, NotIdentifiedInDatabaseException {
+        delete(dbConnection, "id='"+getId()+"'");
+    }
+
+    public List<Object> getAll(DBConnection dbConnection, String condition) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        return dbConnection.getDatabase().selectListObject(dbConnection.getConnection(), getClass(), condition);
+    }
+
+    public List<Object> getAll(DBConnection dbConnection) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        return getAll(dbConnection, "1=1");
+    }
+
+    public Object get(DBConnection dbConnection, String condition) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        return dbConnection.getDatabase().selectObject(dbConnection.getConnection(), getClass(), condition);
+    }
+
+    public Object getById(DBConnection dbConnection, String id) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        return get(dbConnection, "id='"+id+"'");
+    }
+
+    public void historize(DBConnection dbConnection, String action) throws SQLException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        dbConnection.getDatabase().insert(dbConnection.getConnection(), "history", Timestamp.from(Instant.now()), getClass().getSimpleName(), action, toString());
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() throws NotIdentifiedInDatabaseException {
+        if(id == null) throw new NotIdentifiedInDatabaseException(this);
+        return id;
     }
 }
