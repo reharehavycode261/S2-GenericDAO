1f36cb58e43d4b5d109504682943a7ccc78f075c
rehareha261 <161926797+rehareha261@users.noreply.github.com>
2025-10-12 12:04:47 +0300

    Implémentation automatique - feature/0fe544cf-03d895-1203

---
+++ src/database/core/GenericdaoTest.java
@@ -0,0 +1,20 @@
+/**
+ * Tests automatiques pour /var/folders/bv/3lkzxqps10b1h3wh0mb1tg0w0000gn/T/ai_agent_q7tbn0e1/src/database/core/GenericDAO.java
+ * Langage: java
+ * Framework: junit5
+ * 
+ * Ce fichier de test a été généré automatiquement.
+ * TODO: Compléter les tests selon les besoins spécifiques du projet.
+ */
+
+import org.junit.jupiter.api.Test;
+
+
+public class GenericdaoTest {
+    
+    @org.junit.jupiter.api.Test
+    public void testBasic() {
+        // TODO: Ajouter des tests spécifiques
+        Assertions.assert
+    }
+}
---
+++ src/test/MainTest.java
@@ -0,0 +1,20 @@
+/**
+ * Tests automatiques pour /var/folders/bv/3lkzxqps10b1h3wh0mb1tg0w0000gn/T/ai_agent_q7tbn0e1/src/test/Main.java
+ * Langage: java
+ * Framework: junit5
+ * 
+ * Ce fichier de test a été généré automatiquement.
+ * TODO: Compléter les tests selon les besoins spécifiques du projet.
+ */
+
+import org.junit.jupiter.api.Test;
+
+
+public class MainTest {
+    
+    @org.junit.jupiter.api.Test
+    public void testBasic() {
+        // TODO: Ajouter des tests spécifiques
+        Assertions.assert
+    }
+}
