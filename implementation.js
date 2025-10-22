caa2bdefbcef4858cea6d76e2b1c293798fc746b
reharehavycode261 <stagiaire2.vycode@gmail.com>
2025-10-22 12:49:57 +0300

    m

--- implementation.js
+++
@@ -1,3 +0,0 @@
-### Étape 2 : Tester la méthode `updateFields()`
-
-Nous devons maintenant écrire des tests unitaires pour assurer le bon fonctionnement de la méthode à partir de divers scénarios de test.
