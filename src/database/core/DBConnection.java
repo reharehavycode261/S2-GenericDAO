0e551f954c0181c73ad9ec565345b4e771ae95dd
reharehavycode261 <stagiaire2.vycode@gmail.com>
2025-10-22 08:36:23 +0300

    o

--- src/database/core/DBConnection.java
+++ src/database/core/DBConnection.java
@@ -1,7 +1,6 @@
 package database.core;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DBConnection {
@@ -20,43 +19,17 @@ public class DBConnection {
     public void rollback() {
         try {
             getConnection().rollback();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        } catch (SQLException ignored) {}
     }
 
-    /**
-     * Met à jour certains champs spécifiques d'un objet dans la base de données.
-     * 
-     * @param tableName Le nom de la table à mettre à jour
-     * @param fields Les champs à mettre à jour
-     * @param values Les nouvelles valeurs des champs
-     * @param condition La condition pour spécifier les enregistrements à mettre à jour
-     * @throws SQLException Si une erreur SQL survient
-     */
-    public void updateFields(String tableName, String[] fields, Object[] values, String condition) throws SQLException {
-        if (fields.length != values.length) {
-            throw new IllegalArgumentException("Le nombre de champs doit correspondre au nombre de valeurs.");
-        }
+    public void setAutoCommit(boolean state) throws SQLException {
+        getConnection().setAutoCommit(state);
+    }
 
-        StringBuilder sql = new StringBuilder("UPDATE ").append(tableName).append(" SET ");
-
-        for (int i = 0; i < fields.length; i++) {
-            sql.append(fields[i]).append(" = ?");
-            if (i < fields.length - 1) {
-                sql.append(", ");
-            }
-        }
-
-        sql.append(" WHERE ").append(condition);
-
-        try (PreparedStatement stmt = connection.prepareStatement(sql.toString())) {
-            for (int i = 0; i < values.length; i++) {
-                stmt.setObject(i + 1, values[i]);
-            }
-
-            stmt.executeUpdate();
-        }
+    public void close() {
+        try {
+            getConnection().close();
+        } catch (SQLException ignored) {}
     }
 
     public Database getDatabase() {
