9111f1fa3ad82aa0ae926072eb6c11fdb3008c55
reharehavycode261 <stagiaire2.vycode@gmail.com>
2025-10-22 11:00:27 +0300

    p

--- src/database/core/DBConnection.java
+++ src/database/core/DBConnection.java
@@ -1,7 +1,6 @@
 package database.core;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DBConnection {
@@ -20,40 +19,17 @@ public class DBConnection {
     public void rollback() {
         try {
             getConnection().rollback();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        } catch (SQLException ignored) {}
     }
 
-    /**
-     * Met à jour des champs spécifiques d'un enregistrement dans la base de données.
-     * 
-     * @param tableName Le nom de la table à mettre à jour
-     * @param fields Les champs à mettre à jour
-     * @param values Les nouvelles valeurs pour ces champs
-     * @param condition La condition WHERE pour identifier l'enregistrement
-     * @throws SQLException en cas d'erreur SQL
-     */
-    public void updateFields(String tableName, String[] fields, Object[] values, String condition) throws SQLException {
-        if (fields.length != values.length) {
-            throw new IllegalArgumentException("Le nombre de champs et de valeurs doit être le même.");
-        }
+    public void setAutoCommit(boolean state) throws SQLException {
+        getConnection().setAutoCommit(state);
+    }
 
-        StringBuilder query = new StringBuilder("UPDATE " + tableName + " SET ");
-        for (int i = 0; i < fields.length; i++) {
-            query.append(fields[i]).append(" = ?");
-            if (i < fields.length - 1) {
-                query.append(", ");
-            }
-        }
-        query.append(" WHERE ").append(condition);
-
-        try (PreparedStatement stmt = connection.prepareStatement(query.toString())) {
-            for (int i = 0; i < values.length; i++) {
-                stmt.setObject(i + 1, values[i]);
-            }
-            stmt.executeUpdate();
-        }
+    public void close() {
+        try {
+            getConnection().close();
+        } catch (SQLException ignored) {}
     }
 
     public Database getDatabase() {
